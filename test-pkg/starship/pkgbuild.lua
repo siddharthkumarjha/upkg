1f9b302e39665e7b5993b0cebcb1589d18dc0720
Siddharth Kumar Jha <siddharth.k.jha@proton.me>
2025-08-08 03:04:10 +0530

    setting up basic framework

--- test-pkg/starship/pkgbuild.lua
+++ test-pkg/starship/pkgbuild.lua
@@ -1,3 +1,14 @@
+Proto = {
+	git = 1,
+	patch = 2,
+}
+
+-- Create reverse lookup
+ProtoStr = {}
+for k, v in pairs(Proto) do
+	ProtoStr[v] = k
+end
+
 PkgInfo = {
 	name = "starship",
 	desc = "The cross-shell prompt for astronauts",
@@ -13,9 +24,22 @@ CheckDepends = { "python" }
 OptDepends = { "ttf-font-nerd: Nerd Font Symbols preset" }
 
 GitHubLink = "https://github.com" .. "/" .. PkgInfo.name .. "/" .. PkgInfo.name
-Source = { "git+" .. GitHubLink .. ".git#tag=v" .. PkgInfo.ver }
+Source = {
+	{
+		proto = Proto.git,
+		url = GitHubLink,
+		tag = "v" .. PkgInfo.ver,
+		directory = "starship",
+	},
+	{ proto = Proto.patch, file = "./0001-fix-rust-1.89.0-warnings-and-errors-blocking-CI-pipe.patch" },
+	{ proto = Proto.patch, file = "./0002-fix-git-tests-spawning-an-editor.patch" },
+}
 
-Sha256Sums = { "SKIP" }
+Sha256Sums = {
+	"SKIP",
+	"2e66eff0249f87f1deb1dfd0916d1017c1772a05a7627668d8855a3f227908e8",
+	"41e085267c1a8c60b29442a8376c4cf2c1f98f658b13ff17370887413047e7f4",
+}
 
 function Run(cmd)
 	local handle = io.popen(cmd .. " 2>&1")
@@ -29,21 +53,32 @@ function GetHost(rustc_output)
 end
 
 function Prepare()
+	for _, s in ipairs(Source) do
+		if s.proto == Proto.patch then
+			local patch_cmd = ProtoStr[s.proto] .. " -d starship -p1 < " .. s.file
+			print(patch_cmd)
+			os.execute(patch_cmd)
+		end
+	end
+
 	local rust_version = Run("rustc -vV")
 	local target_host = GetHost(rust_version)
 
 	local cargo_cmd = "cargo fetch --locked --target " .. target_host .. " --manifest-path starship/Cargo.toml"
+	print(cargo_cmd)
 	os.execute(cargo_cmd)
 end
 
 function Build()
 	local env = 'CARGO_TARGET_DIR=target CFLAGS+=" -ffat-lto-objects"'
 	local cargo_cmd = "cargo build --release --frozen --manifest-path starship/Cargo.toml"
+	print(cargo_cmd)
 	os.execute(env .. " " .. cargo_cmd)
 end
 
 function Check()
 	local cargo_cmd = "cargo test --frozen --manifest-path starship/Cargo.toml"
+	print(cargo_cmd)
 	os.execute(cargo_cmd)
 end
 
@@ -61,31 +96,66 @@ function Package()
 		.. "/usr/share/fish/vendor_completions.d/starship.fish",
 		"./target/release/starship completions zsh > " .. PkgDir .. "/usr/share/zsh/site-functions/_starship",
 	}, "\n")
+	print(install_starship)
 	os.execute(install_starship)
 end
 
-function ParseSourceUrl(source)
-	local proto, rest = string.match(source, "^(.-)%+(.*)$")
-	local url, tag = string.match(rest, "^(.-)#tag=(.*)$")
-	return {
-		proto = proto,
-		full = source,
-		url = url or rest,
-		tag = tag,
-	}
+local function get_filename_from_url(url)
+	-- Strip query string and fragment
+	local cleaned = url:match("^[^?#]+")
+	-- Extract last part after final slash
+	return cleaned:match("^.+/(.+)$")
+end
+
+function Verify()
+	for i, s in ipairs(Source) do
+		local file_name
+		if s.file ~= nil then
+			file_name = s.file
+		elseif s.url ~= nil then
+			file_name = get_filename_from_url(s.url)
+		else
+			error("no file passed to source?")
+		end
+
+		if Sha256Sums[i] ~= "SKIP" then
+			local actual_sha = Run("sha256sum " .. file_name):match("^([a-f0-9]+)")
+			if actual_sha ~= Sha256Sums[i] then
+				error(
+					"Sha256Sums mismatch, expected: "
+					.. Sha256Sums[i]
+					.. " got "
+					.. actual_sha
+					.. " for file "
+					.. file_name
+				)
+			else
+				print("sha256 for " .. file_name .. " successfully validated")
+			end
+		else
+			print("for ", file_name, " SKIP sha check requested")
+		end
+	end
+end
+
+function Fetch()
+	for _, s in ipairs(Source) do
+		if s.proto == Proto.git then
+			local clone_cmd = ProtoStr[s.proto] .. " clone " .. s.url .. " -b " .. s.tag .. " " .. s.directory
+			print(clone_cmd)
+			os.execute(clone_cmd)
+		end
+	end
 end
 
 -- control flow
 -- download source -> verify() -> extract source -> prepare() -> build() -> check() -> package()
 function Test_fn()
 	PkgDir = "/home/siddharth/tst/"
-	for _, s in ipairs(Source) do
-		local parsed_url = ParseSourceUrl(s)
-		local clone_cmd = parsed_url.proto .. " clone " .. parsed_url.url .. " -b " .. parsed_url.tag
-		os.execute(clone_cmd)
-	end
+	Fetch()
+	Verify()
 	Prepare()
 	Build()
-	-- Check()
+	Check()
 	Package()
 end
